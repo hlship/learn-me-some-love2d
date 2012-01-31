1e4150087991a8d8143dd7a93e744a832ca8cff9
Howard M. Lewis Ship <hlship@gmail.com>
2012-01-30 18:00:32 -0800

    Rebuild the main script as a harness around the params module

--- src/main.lua
+++ src/main.lua
@@ -2,104 +2,40 @@ local g = love.graphics
 
 local tween = require("tween")
 
-require("middleclass")
+local params = require("params")
 
-local images = {}
-
-local grid = {
-   cell = {
-      width = 101, -- width of individual cell
-      height = 171, -- height of individual cell
-      topOverlap = 49,
-      bottomOverlap = 40
-   },
-   frame = {
-      top = 40,
-      left = 8
-   },
-   width = 10, -- number of cells across
-   height = 8, -- number of cells down
-}
-
-local player = {
-   gridX = 3,
-   gridY = 3,
-   moving = false
-}
-
-local function toScreenXY(gridX, gridY, isPlayer)
-   local c = grid.cell
-
-   local vertOffset = isPlayer and (c.bottomOverlap + c.topOverlap) or c.topOverlap
-
-   return gridX * c.width + grid.frame.left, gridY * (c.height - c.topOverlap - c.bottomOverlap) - vertOffset + grid.frame.top
-end
+myParam = 87
 
 function love.load()
-   love.keyboard.setKeyRepeat(10, 200)
+   params.param("myParam", 1, 100)
+end
 
-   images.boy = g.newImage("planet-cute/Character Boy.png")
-   images.stone = g.newImage("planet-cute/Stone Block.png")
-
-   player.screenX, player.screenY = toScreenXY(player.gridX, player.gridY, true)
+function love.update(dt)
+   tween.update(dt)
 end
 
 function love.draw()
 
-   g.setBackgroundColor(0, 127, 0)
+   g.setBackgroundColor(0, 200, 0)
 
-   -- Draw in the background
-   for x = 0, grid.width - 1 do
-      for y = 0, grid.height -1 do
-         screenX, screenY = toScreenXY(x, y)
-         g.draw(images.stone, screenX, screenY)
-      end
+   drawGrid()
+
+   params.draw() -- always last
+end
+
+function drawGrid()
+   g.setColor(0, 0, 0)
+   g.setLineWidth(2)
+
+   local width, height = g.getWidth(), g.getHeight()
+
+   for x = 0, width, 25 do
+      g.line(x, 0, x, height)
    end
 
-   g.draw(images.boy, player.screenX, player.screenY)
-
-end
-
-function playerMoveComplete()
-   player.moving = false
-end
-
-function movePlayer(xoffset, yoffset)
-
-   player.gridX = player.gridX + xoffset
-   player.gridY = player.gridY + yoffset
-
-   local target = {}
-
-   target.screenX, target.screenY = toScreenXY(player.gridX, player.gridY, true)
-
-   if xoffset == 0 then target.screenX = null end
-   if yoffset == 0 then target.screenY = null end
-
-   player.moving = true
-
-   tween.start(.5, player, target, tween.easing.inOutQuad, playerMoveComplete)
-end
-
-function movePlayerUp()
-   if player.gridY ~= 0 then movePlayer(0, -1) end
-end
-
-function movePlayerLeft()
-   if player.gridX ~= 0 then movePlayer(-1, 0) end
-end
-
-function movePlayerRight()
-   if player.gridX + 1 < grid.width then movePlayer(1, 0) end
-end
-
-function movePlayerDown()
-   if player.gridY + 1 < grid.height then movePlayer(0, 1) end
-end
-
-
-function love.update(dt)
-   tween.update(dt)
+   for y = 0, height, 25 do
+      g.line(0, y, width, y)
+   end
 end
 
 function love.keypressed(key, unicode)
@@ -107,7 +43,6 @@ function love.keypressed(key, unicode)
       love.event.push("q") -- "q" is a special event name to quit the game immediately and cleanly
       return
    end
-
 
    if key == "f" then
       g.toggleFullscreen()
@@ -120,17 +55,5 @@ function love.keypressed(key, unicode)
       love.load()
       return
    end
-
-   if not player.moving then
-
-      if key == "up" then movePlayerUp() return end
-
-      if key == "down" then movePlayerDown() return end
-
-      if key == "right" then movePlayerRight() return end
-
-      if key == "left" then movePlayerLeft() return end
-   end
-
 
 end
