5ef4ed209dac01454b88066c0d6fec23a833cef8
Howard M. Lewis Ship <hlship@gmail.com>
2012-02-01 13:07:13 -0800

    Parameter panel layout improvements
    Use a fixed size image, stretched out, to form the background of the panel
    Fiddle with the offsets to get things laid out correcty

--- src/params.lua
+++ src/params.lua
@@ -5,6 +5,8 @@
 
 require "middleclass"
 
+local paneImage
+
 -- The namespace to be returned by module
 local params = {}
 
@@ -16,7 +18,11 @@ local Pane = class "Pane"
 
 local WIDTH = 200 -- Overall width of a pane
 local HEIGHT = 100 -- Height of a pane
-local INSET = 20 -- Inset for the drag control
+local TEXT_INSET = 10 -- Inset for the text
+local SLIDER_INSET = 20 -- Inset for the slider control
+
+local WHITE = {255, 255, 255}
+local ORANGE = {255, 138, 0}
 
 function Pane:initialize(x, y, property, min, max)
    self.x, self.y = x, y
@@ -24,8 +30,8 @@ function Pane:initialize(x, y, property, min, max)
 
    self.min, self.max = min, max
 
-   self.minX = x + INSET
-   self.maxX = x + WIDTH - INSET
+   self.minX = x + SLIDER_INSET
+   self.maxX = x + WIDTH - SLIDER_INSET
 
    self.pixelIncrement = (self.maxX - self.minX) / (max - min)
 
@@ -35,24 +41,28 @@ function Pane:initialize(x, y, property, min, max)
    self.nibX = self.minX + (value - min) * self.pixelIncrement
 end
 
-function Pane:draw()
-   g.setColor (0, 0, 0, 190) -- 75% opacity
-   g.rectangle("fill", self.x, self.y, WIDTH, HEIGHT)
-   g.setColor(255, 255, 255)
-   g.setLineWidth(2)
-   g.rectangle("line", self.x, self.y, WIDTH, HEIGHT)
 
-   g.print(self.property, self.x + 5, self.y + 5)
+function Pane:draw()
+
+   g.setColor(WHITE)
+
+   g.draw(paneImage, self.x, self.y)
+
+   g.print(self.property, self.x + TEXT_INSET, self.y + TEXT_INSET)
 
    local value = tostring(_G[self.property])
 
    local textWidth = g.getFont():getWidth(value)
 
-   g.setColor(255, 138, 0)
-   g.print(value, self.x + WIDTH - 5 - textWidth, self.y + 6)
+   g.setColor(ORANGE)
+
+   g.print(value, self.x + WIDTH - TEXT_INSET - textWidth, self.y + TEXT_INSET)
 
 end
 
+local function setup()
+   paneImage = paneImage or g.newImage("params/param-pane-bezel.png")
+end
 
 local nextPaneY = 2
 local panes = {}
@@ -61,9 +71,12 @@ local panes = {}
 -- range across the floating values between min and max
 -- This is typically invoked from the love.load() callback
 function params.param(property, min, max) 
+
+   setup()
+
    table.insert(panes, Pane:new(2, nextPaneY, property, min, max))
 
-   nextPaneY = nextPaneY - HEIGHT + 10
+   nextPaneY = nextPaneY + HEIGHT
 end
 
 -- Must be called at the end of the love.draw() callback to draw the 
